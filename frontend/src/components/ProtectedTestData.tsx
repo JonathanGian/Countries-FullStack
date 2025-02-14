599d6ef4c0da4e5b3245d5e895b6677d8f9d14cc
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-02-14 10:31:21 +0200

    Add AuthContext, Navigation, ProtectedTestData, Auth folder. Update App.tsx

---
+++ frontend/src/components/ProtectedTestData.tsx
@@ -0,0 +1,8 @@
+const ProtectedTestData = () => {
+const [data, setData] = useState<string | null>(null);
+const [error, setError] = useState<string | null>(null);
+const [loading, setLoading] = useState<boolean>(false);
+
+
+
+}
